078d4066f24115fd1c89595b79a136696ead0db2
Aman Pratap Singh <aman.singh@esyasoft.com>
2025-09-17 17:52:50 +0530

    DLMS Practice Updated First commit

---
+++ DLMS/dlms_practice/dlms_practice/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Assignment - DLMS/COSEM communication example
+in C# .net, demonstrating how to:
+
+Define and pack an OBIS code
+
+Construct a GET request APDU*/ 
+
+using DlmsCosemExample;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // OBIS code for Active Energy Import (1-0:1.8.0.255)
+        var obis = new ObisCode(1, 0, 1, 8, 0, 255);
+
+        // Class ID for Register object is 3
+        ushort classId = 3;
+        byte attributeId = 2; // e.g., value attribute
+
+        byte[] apdu = GetRequestApdu.CreateGetRequest(obis, classId, attributeId);
+
+        Console.WriteLine("OBIS Code: " + obis);
+        Console.WriteLine("GET Request APDU: " + BitConverter.ToString(apdu));
+    }
+}
+}
